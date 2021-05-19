e6a22fbfe5743b567fa48ae83e9f4708fd0c091c
Artis <mundurs.artis@gmail.com>
2021-05-19 10:12:12 +0200

    Downlink updates
    Still broken, but less violently

--- main.c
+++ main.c
@@ -27,50 +27,34 @@
 // define semaphore handle
 SemaphoreHandle_t xTestSemaphore;
 bool initializedTemp = false;
-MessageBufferHandle_t messageBuffer;
-
+MessageBufferHandle_t downLinkMessageBufferHandle;
 // Prototype for LoRaWAN handler
 void lora_handler_initialise(UBaseType_t lora_handler_task_priority);
 
 /*-----------------------------------------------------------*/
 void create_tasks_and_semaphores(void)
 {
-	// Semaphores are useful to stop a Task proceeding, where it should be paused to wait,
-	// because it is sharing a resource, such as the Serial port.
-	// Semaphores should only be used whilst the scheduler is running, but we can set it up here.
-	if ( xTestSemaphore == NULL )  // Check to confirm that the Semaphore has not already been created.
-	{
-		xTestSemaphore = xSemaphoreCreateMutex();  // Create a mutex semaphore.
-		if ( ( xTestSemaphore ) != NULL )
-		{
-			xSemaphoreGive( ( xTestSemaphore ) );  // Make the mutex available for use, by initially "Giving" the Semaphore.
-		}
-	}
+lora_handler_initialise(3);
+printf("Created the message buffer");
+lora_DownLinkHandler_Create(tskIDLE_PRIORITY + 1, configMINIMAL_STACK_SIZE, downLinkMessageBufferHandle);
 }
 
 
 void initialiseSystem()
 {
+	downLinkMessageBufferHandle = xMessageBufferCreate(sizeof(lora_driver_payload_t)*2);
 	// Set output ports for leds used in the example
 	//DDRA |= _BV(DDA0) | _BV(DDA7);
 
 	// Make it possible to use stdio on COM port 0 (USB) on Arduino board - Setting 57600,8,N,1
 	stdio_initialise(ser_USART0);
-	// Let's create some tasks
+
+
+	status_leds_initialise(5); 
+	
+	lora_driver_initialise(ser_USART1, downLinkMessageBufferHandle);
 	create_tasks_and_semaphores();
 
-	
-
-	// vvvvvvvvvvvvvvvvv BELOW IS LoRaWAN initialisation vvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
-	// Status Leds driver
-	status_leds_initialise(5); // Priority 5 for internal task
-	// Initialise the LoRaWAN driver without down-link buffer
-	//messageBuffer = xMessageBufferCreate(14);
-	lora_driver_initialise(1, messageBuffer);
-	// Create LoRaWAN task and start it up with priority 3
-	lora_handler_initialise(3);
-	printf("Created the message buffer");
-	//lora_DownLinkHandler_Create(3, configMINIMAL_STACK_SIZE, messageBuffer);
 }
 
 
